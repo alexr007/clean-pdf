d68747ad6ed9b7afb2f57b30d7cb0ea39f8f1ee5
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-13 11:59:40 +0200

    pdf added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Param, Res } from "@nestjs/common";
 import { AppService } from './app.service';
+import fs = require('fs');
+
+function nameById(id: number) {
+  return `file_${id}.pdf`;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  inv000(): string {
+    return 'Hi';
+  }
+
+  @Get('/inv/:id')
+  async inv001(@Param('id') id: number): Promise<string> {
+    const pdf = await this.appService.createPDF();
+
+    return new Promise(((resolve, reject) => {
+      fs.writeFile(nameById(id), pdf.content, (err) => {
+        if (err) {
+          return reject(err.toString());
+        }
+        return resolve(`${pdf.spent / 1000}s`);
+      });
+    }));
+  }
+
+  @Get('/pdf/:id')
+  @Header('Content-type', 'application/pdf')
+  pdf001(@Param('id') id: number, @Res() res) {
+    const filename = nameById(id);
+    res.sendFile(filename, {root: '.'});
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+const puppeteer = require('puppeteer');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World! ' + new Date() + ' JUST';
+  async createPDF() {
+    const startedAt = new Date();
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://google.com', { waitUntil: 'networkidle0' });
+    const pdf = await page.pdf({ format: 'A4' });
+    await browser.close();
+    const doneAt = new Date();
+    // @ts-ignore
+    const delta = doneAt - startedAt;
+    return {
+      content: pdf,
+      spent: delta
+    }
   }
 }
