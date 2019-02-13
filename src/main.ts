bd8969e0c7cdc55d86b271c50ef545cd21eee96c
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-13 12:07:12 +0200

    minor

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`);
+  });
+
 }
 bootstrap();
