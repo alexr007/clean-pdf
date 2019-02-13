a30edef7015e4dd3d28f1061359837422f82a85a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-13 10:40:32 +0200

    JS + TS + tsconfig

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, resp) => {
-    resp.send('It Works (JS version)');
+    resp.send('It Works (TS version)');
 });
 
 app.listen(PORT, () => {
