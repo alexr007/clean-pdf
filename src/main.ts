c3c0edf5b2ea3d8bed3418e64139dbd005f4961f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-13 12:16:07 +0200

    pdf added

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => {
-    console.log(`App started on port ${PORT}`);
+    console.log(`App started on port ${PORT}!`);
   });
 
 }
